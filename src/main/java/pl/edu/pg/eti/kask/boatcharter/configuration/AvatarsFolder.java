3aa873d8b19e84cd368324088ff528128af15e95
tomkielczewski <tomasz.kielczewski@antrez.pl>
2021-11-26 18:28:37 +0100

    Add language locale

--- src/main/java/pl/edu/pg/eti/kask/boatcharter/configuration/AvatarsFolder.java
+++ src/main/java/pl/edu/pg/eti/kask/boatcharter/configuration/AvatarsFolder.java
@@ -2,8 +2,5 @@ package pl.edu.pg.eti.kask.boatcharter.configuration;
 
 public class AvatarsFolder {
     public static final String AVATARS_FOLDER = "C:\\Users\\Tomek\\Repos\\Studia\\boat-charter\\src\\main\\resources\\pl\\edu\\pg\\eti\\kask\\boatcharter\\configuration";
-//    public static final String AVATARS_FOLDER = "src/main/resources/pl/edu/pg/eti/kask/boatcharter/configuration";
-//    public static final String AVATARS_FOLDER = "/home/student/165625/boat-charter/src/main/resources/pl/edu/pg/eti/kask/boatcharter/configuration";
-//public static final String AVATARS_FOLDER = "C:\\Users\\Tomek\\Repos\\Studia\\boat-charter\\src\\main\\resources\\pl\\edu\\pg\\eti\\kask\\boatcharter\\configuration";
 
 }
